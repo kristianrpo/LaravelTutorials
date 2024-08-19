b5b72ed8f350d10e2415dca332c75f397af7605a
kristian <krestrepoo@eafit.edu.co>
2024-08-19 12:04:15 -0500

    feat: add product id validation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ProductController extends Controller
 {
     public static $products = [
-        ['id'=>'1', 'name'=>'TV', 'description'=>'Best TV', 'price'=>1000],
-        ['id'=>'2', 'name'=>'iPhone', 'description'=>'Best iPhone', 'price'=>800],
-        ['id'=>'3', 'name'=>'Chromecast', 'description'=>'Best Chromecast', 'price'=>50],
-        ['id'=>'4', 'name'=>'Glasses', 'description'=>'Best Glasses', 'price' => 100]
+        ['id' => '1', 'name' => 'TV', 'description' => 'Best TV', 'price' => 1000],
+        ['id' => '2', 'name' => 'iPhone', 'description' => 'Best iPhone', 'price' => 800],
+        ['id' => '3', 'name' => 'Chromecast', 'description' => 'Best Chromecast', 'price' => 50],
+        ['id' => '4', 'name' => 'Glasses', 'description' => 'Best Glasses', 'price' => 100]
     ];
 
     public function index(): View
@@ -23,21 +24,34 @@ class ProductController extends Controller
         return view('product.index')->with('viewData', $viewData);
     }
 
-    public function show(string $id) : View
+    public function show(string $id): View | RedirectResponse
     {
-        $viewData = [];
-        $product = ProductController::$products[$id-1];
-        $viewData['title'] = $product['name'].' - Online Store';
-        $viewData['subtitle'] =  $product['name'].' - Product information';
-        $viewData['product'] = $product;
-        return view('product.show')->with('viewData', $viewData);
+        $idExist = false;
+        foreach (ProductController::$products as $product) {
+            if ($product['id'] == $id) {
+                $idExist = true;
+                break;
+            } else {
+                continue;
+            }
+        }
+        if ($idExist) {
+            $viewData = [];
+            $product = ProductController::$products[$id - 1];
+            $viewData['title'] = $product['name'] . ' - Online Store';
+            $viewData['subtitle'] =  $product['name'] . ' - Product information';
+            $viewData['product'] = $product;
+            return view('product.show')->with('viewData', $viewData);
+        } else {
+            return redirect()->route('home.index');
+        }
     }
 
     public function create(): View
     {
         $viewData = []; //to be sent to the view
         $viewData['title'] = 'Create product';
-        return view('product.create')->with('viewData',$viewData);
+        return view('product.create')->with('viewData', $viewData);
     }
 
     public function save(Request $request)
