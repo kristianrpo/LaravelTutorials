a9117175731edeb3e9b6c01dbdd86da4702fe226
kristian <krestrepoo@eafit.edu.co>
2024-08-19 11:32:24 -0500

    refactor: change double quotes into single quotes

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section("title", $viewData["title"])
+@section('title', $viewData['title'])
 @section('content')
 <div class="container">
   <div class="row justify-content-center">
