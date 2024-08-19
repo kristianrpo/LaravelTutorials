183b26926a82b41a14804e435355a447543499a6
kristian <krestrepoo@eafit.edu.co>
2024-08-19 12:14:15 -0500

    feat: add validation to change product name color according to the price

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -9,9 +9,15 @@
     </div>
     <div class="col-md-8">
       <div class="card-body">
+        @if($viewData['product']['price'] > 100)
+        <h5 class="card-title text-danger">
+          {{ $viewData['product']['name'] }}
+        </h5>
+        @else
         <h5 class="card-title">
           {{ $viewData['product']['name'] }}
         </h5>
+        @endif
         <p class="card-text">{{ $viewData['product']['description'] }}</p>
         <p class="card-text"> <b>Price: </b> {{ $viewData['product']['price'] }} <b>$</b> </p>
       </div>
